7737809b37b0545373e050190f75919058375145
kdthr78 <ian@bjharvest.co.uk>
2023-03-13 22:13:17 +0000

    Added basic movement script
    Added a movement script, a plane, cube controllable character and sphere object

--- Evolution Battle Royale/Assets/Movement.cs
+++ Evolution Battle Royale/Assets/Movement.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private Rigidbody body;
+    private Vector3 playerVelocity;
+    public float speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = gameObject.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(xMove*speed, body.velocity.y, zMove*speed);
+        body.velocity = move;
     }
 }
